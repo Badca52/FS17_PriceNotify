1e19a0b2d7463d14e143cd95ef5c68d921f27574
Jeff Laughlin <jmlaughlin38@gmail.com>
2018-03-03 14:56:56 -0500

    Fixed Wheat Price

--- scripts/Alarm/alarmLibrary.lua
+++ scripts/Alarm/alarmLibrary.lua
@@ -2,7 +2,7 @@ alarmLibrary = {}
 
 function alarmLibrary.load()
   priceNotify.alarmLibrary = {
-    ["Wheat"] = { threshold = 1200, active = false, curMaxPrice = 0 },
+    ["Wheat"] = { threshold = 700, active = false, curMaxPrice = 0 },
     ["Barley"] = { threshold = 475, active = false, curMaxPrice = 0 },
     ["Canola"] = { threshold = 1300, active = false, curMaxPrice = 0 },
     ["Sunflowers"] = { threshold = 1100, active = false, curMaxPrice = 0 },
